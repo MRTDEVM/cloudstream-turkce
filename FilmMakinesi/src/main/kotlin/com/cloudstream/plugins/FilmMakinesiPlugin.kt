caaa73960f13fac51d1501b28f59639ada366886
MRTDEVM <mertgurnil656@gmail.com>
2026-08-19 01:00:56 +0300

    Fix: FilmMakinesiPlugin Context importu duzeltildi (android.content.Context)

--- FilmMakinesi/src/main/kotlin/com/cloudstream/plugins/FilmMakinesiPlugin.kt
+++ FilmMakinesi/src/main/kotlin/com/cloudstream/plugins/FilmMakinesiPlugin.kt
@@ -2,7 +2,7 @@ package com.cloudstream.plugins
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import android.context.Context
+import android.content.Context
 
 @CloudstreamPlugin
 class FilmMakinesiPlugin : Plugin() {
